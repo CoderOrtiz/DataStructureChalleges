fb39cdb2c5771098506eb4167dc2199bbfec8f59
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-08-10 18:13:36 -0400

    Practicing using Stacks

--- node.js
+++ node.js
@@ -1,5 +1,53 @@
 ////////////// August 10, 2022 /////////////////
 
+//// Practice Working with Stacks ////
+
+// Creating a Stack
+
+const Stack = function () {
+  this.count = 0;
+  this.storage = {};
+
+  // Adds a Value onto the End of the Stack
+  this.push = function (value) {
+    this.storage[this.count] = value;
+    this.count++;
+  };
+
+  // Removes and Returns the Value at the end of the Stack
+  this.pop = function () {
+    if (this.count === 0) {
+      return undefined;
+    }
+
+    this.count--;
+    const result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  };
+
+  this.size = function () {
+    return this.count;
+  };
+
+  // Returns the value at the End of the Stack
+  this.peek = function () {
+    return this.storage[this.count - 1];
+  };
+};
+
+const myStack = new Stack();
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+myStack.push("HelloWorld");
+console.log(myStack.size());
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+
 // Palindrome Challenge
 
 const letters = [];
@@ -20,4 +68,4 @@ function palindrome(word) {
     console.log(`${word} is NOT a palindrone`);
   }
 }
-palindrome("racecar");
+// palindrome("racecar");
