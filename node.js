ede8479bf99145b125ae3759f1518480dfdac8e8
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-08-10 20:25:53 -0400

    Practicing Using Sets

--- node.js
+++ node.js
@@ -1,9 +1,108 @@
 ////////////// August 10, 2022 /////////////////
 
-//// Practice Working with Stacks ////
+/// Getting Familar with Data Stuctures ///
+
+// Practice Working with Sets //
+
+function mySet() {
+  // Collection will Hold the Set
+  const collection = [];
+
+  // This Method will Check for the Presence of an Element and Return a Boolean
+  this.has = function (element) {
+    return collection.indexOf(element) !== -1;
+  };
+
+  // This Method will Return all the Values in the Set
+  this.values = function () {
+    return collection;
+  };
+
+  // this Method will Add an Element to the Set
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
+
+  // This Method will Remove an Element from a Set
+  this.remove = function (element) {
+    if (this.has(element)) {
+      index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+
+  // This Method will Return the Size of the Collection
+  this.size = function () {
+    return colection.length;
+  };
+
+  // This Method will Return the Union of the Two Sets
+  this.union = function (otherSet) {
+    const unionSet = new mySet();
+    const firstSet = this.values();
+    const secondSet = otherSet.values();
+    firstSet.forEach(function (e) {
+      unionSet.add(e);
+    });
+    return unionSet;
+  };
+
+  //   This Method will Return the Intersection of Two Sets as a New Set
+  this.intersection = function (otherSet) {
+    const intersectionSet = new mySet();
+    const firstSet = this.values();
+    firstSet.forEach(function (e) {
+      if (otherSet.has(e)) {
+        intersectionSet.add(e);
+      }
+    });
+    return intersectionSet;
+  };
+
+  //   This Method will Return the Difference of Two Sets as a New Set
+  this.difference = function (otherSet) {
+    const differenceSet = new mySet();
+    const firstSet = this.values();
+    firstSet.forEach(function (e) {
+      if (!otherSet.has(e)) {
+        differenceSet.add(e);
+      }
+    });
+    return differenceSet;
+  };
+
+  //   This Method will Test if the Set is a Subset of a Different Set
+  this.subset = function (otherSet) {
+    const firstSet = this.values();
+    return firstSet.every(function (value) {
+      return otherSet.has(value);
+    });
+  };
+}
+
+const setA = new mySet();
+const setB = new mySet();
+
+setA.add("A");
+setB.add("B");
+setB.add("C");
+setB.add("A");
+setB.add("D");
+console.log(setA.subset(setB));
+console.log(setA.intersection(setB).values());
+console.log(setB.difference(setA).values());
+
+// Practice Working with Stacks //
 
 // Creating a Stack
 
+// "Stack" is Capitalized because this is a Constructor Function
 const Stack = function () {
   this.count = 0;
   this.storage = {};
@@ -36,17 +135,19 @@ const Stack = function () {
   };
 };
 
-const myStack = new Stack();
-myStack.push(1);
-myStack.push(2);
-console.log(myStack.peek());
-console.log(myStack.pop());
-console.log(myStack.peek());
-myStack.push("HelloWorld");
-console.log(myStack.size());
-console.log(myStack.peek());
-console.log(myStack.pop());
-console.log(myStack.peek());
+// Making a Constructor Function
+// "new" Makes a New Object
+// const myStack = new Stack();
+// myStack.push(1);
+// myStack.push(2);
+// console.log(myStack.peek());
+// console.log(myStack.pop());
+// console.log(myStack.peek());
+// myStack.push("HelloWorld");
+// console.log(myStack.size());
+// console.log(myStack.peek());
+// console.log(myStack.pop());
+// console.log(myStack.peek());
 
 // Palindrome Challenge
 
