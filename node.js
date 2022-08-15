0b25ba4f31a93a56e65c84ba351989df55656fe2
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-08-14 21:12:39 -0400

    Practicing using Linked Lists

--- node.js
+++ node.js
@@ -4,6 +4,143 @@ const res = require("express/lib/response");
 
 ////////////// August 14, 2022 /////////////////
 
+//// Linked List ////
+
+function LinkedList() {
+  let length = 0;
+  let head = null;
+
+  let Node = function (element) {
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function () {
+    return length;
+  };
+
+  this.head = function () {
+    return this.head;
+  };
+
+  this.add = function (element) {
+    let node = new Node(element);
+    if (head === null) {
+      head = node;
+    } else {
+      currentNode = head;
+
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = node;
+    }
+    length++;
+  };
+
+  this.remove = function (element) {
+    let currentNode = head;
+    let previousNode;
+    if (currentNode.element === element) {
+      head = currentNode.next;
+    } else {
+      while (currentNode.element !== element) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+    }
+    length--;
+  };
+
+  this.isEmpty = function () {
+    return length === 0;
+  };
+
+  this.indexOf = function (element) {
+    let currentNode = head;
+    let index = -1;
+
+    while (currentNode) {
+      index++;
+      if (currentNode.element === element) {
+        return index;
+      }
+      currentNode = currentNode.next;
+    }
+    return -1;
+  };
+
+  this.elementAt = function (index) {
+    let currentNode = head;
+    let count = 0;
+    while (count < index) {
+      count++;
+      currentNode = currentNode.next;
+    }
+    return currentNode.element;
+  };
+
+  this.addAt = function (index, element) {
+    let node = Node(element);
+
+    let currentNode = head;
+    let previousNode;
+    let currentIndex = 0;
+
+    if (index > length) {
+      return false;
+    }
+
+    if (index === 0) {
+      node.next = currentNode;
+      head = node;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      node.next = currentNode;
+      previousNode.next = node;
+    }
+    length++;
+  };
+  this.removeAt = function (index) {
+    let currentNode = head;
+    let previousNode;
+    let currentIndex = 0;
+    if (index < 0 || index >= length) {
+      return null;
+    }
+    if (index === 0) {
+      head = currentNode.next;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+    }
+    length--;
+    return currentNode.element;
+  };
+}
+
+let conga = new LinkedList();
+conga.add("Kitten");
+conga.add("Puppy");
+conga.add("Dog");
+conga.add("Cat");
+conga.add("Fish");
+
+console.log(conga.size());
+console.log(conga.removeAt(3));
+console.log(conga.elementAt(3));
+console.log(conga.indexOf("Puppy"));
+console.log(conga.size());
+
 //// Hash Tables ////
 
 const hash = (string, max) => {
@@ -67,16 +204,16 @@ let HashTable = function () {
   };
 };
 
-console.log(hash("quincy", 10));
+// console.log(hash("quincy", 10));
 
-let ht = new HashTable();
-ht.add("beau", "person");
-ht.add("fido", "dog");
-ht.add("rex", "dinosour");
-ht.add("tux", "penguin");
+// let ht = new HashTable();
+// ht.add("beau", "person");
+// ht.add("fido", "dog");
+// ht.add("rex", "dinosour");
+// ht.add("tux", "penguin");
 
-console.log(ht.lookup("tux"));
-ht.print();
+// console.log(ht.lookup("tux"));
+// ht.print();
 //// Traversal & Height of a Binary Search Tree  ////
 
 class Node {
